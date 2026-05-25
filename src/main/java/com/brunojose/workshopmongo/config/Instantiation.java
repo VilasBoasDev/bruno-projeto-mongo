c95f45e9c77ccf36759236b6d5a307e66ba790be
Bruno José Vilas Bôas <bruuno28.28@gmail.com>
2026-05-24 21:10:09 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/brunojose/workshopmongo/config/Instantiation.java
+++ src/main/java/com/brunojose/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.brunojose.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.brunojose.workshopmongo.domain.Post;
 import com.brunojose.workshopmongo.domain.User;
+import com.brunojose.workshopmongo.repository.PostRepository;
 import com.brunojose.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,12 +18,19 @@ public class Instantiation implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
         
+    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    	sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+    	
         // Limpa a coleção antes de inserir (garante IDs consistentes)
         userRepository.deleteAll();
+        postRepository.deleteAll();
         
         // Cria usuários com IDs fixos (ou deixe null para o MongoDB gerar)
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -28,6 +39,11 @@ public class Instantiation implements CommandLineRunner {
         
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
         
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
         // Agora imprima os IDs gerados para você saber quais usar
         System.out.println("=== USUÁRIOS INSERIDOS ===");
         for (User u : userRepository.findAll()) {
