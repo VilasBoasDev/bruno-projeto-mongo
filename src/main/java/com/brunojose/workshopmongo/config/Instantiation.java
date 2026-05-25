378cea9b2322384c6ac86deeb1dc54de83ff153f
Bruno José Vilas Bôas <bruuno28.28@gmail.com>
2026-05-24 21:28:23 -0300

    Projeção dos dados do autor com dto

--- src/main/java/com/brunojose/workshopmongo/config/Instantiation.java
+++ src/main/java/com/brunojose/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.brunojose.workshopmongo.domain.Post;
 import com.brunojose.workshopmongo.domain.User;
+import com.brunojose.workshopmongo.dto.AuthorDTO;
 import com.brunojose.workshopmongo.repository.PostRepository;
 import com.brunojose.workshopmongo.repository.UserRepository;
 
@@ -39,11 +40,12 @@ public class Instantiation implements CommandLineRunner {
         
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
         
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
         postRepository.saveAll(Arrays.asList(post1, post2));
+        
         // Agora imprima os IDs gerados para você saber quais usar
         System.out.println("=== USUÁRIOS INSERIDOS ===");
         for (User u : userRepository.findAll()) {
